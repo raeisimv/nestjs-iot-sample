11131d570b8b156d44ee3677e9d13206add4d864
Morteza Raeisi <mrraeisi@outlook.com>
2025-08-16 23:36:38 +0330

    impl producing mocked record periodically

--- producer/src/index.js
+++ producer/src/index.js
@@ -1,8 +1,14 @@
 import {generateRandomRecord} from "./mocker.js";
+import {publish} from "./rabbitmq.js";
 
 async function main() {
-    const mock = generateRandomRecord();
-    console.log(mock);
+    while (true) {
+        const mockedRecord = generateRandomRecord();
+        await publish('iot.signal', mockedRecord);
+
+        const sleepTimeout = Math.floor((Math.random() * 1000) + 500);
+        await new Promise(resolve => setTimeout(resolve, sleepTimeout));
+    }
 }
 
 main()
---
+++ producer/src/rabbitmq.js
@@ -0,0 +1,4 @@
+export async function publish(routingKey, payload) {
+    // log it for now
+    console.log('publishing', routingKey, payload)
+}
