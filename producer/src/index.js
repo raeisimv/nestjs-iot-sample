dcf65cdbc6b5e72a1796b85239bc64b2e3ae20e9
Morteza Raeisi <mrraeisi@outlook.com>
2025-08-25 21:28:41 +0330

    (producer) add a simple retry strategy for reconnecting to RabbitMQ

--- producer/src/index.js
+++ producer/src/index.js
@@ -3,13 +3,27 @@ import {initRabbitMQ} from "./rabbitmq.js";
 
 async function main() {
     console.log('initializing...');
-    const publish = await initRabbitMQ();
 
-    console.log('start generating IoT signals ...')
+    let publish;
+    for (let i = 0; i < 50; i++) {
+        try {
+            publish = await initRabbitMQ();
+            break;
+        } catch (err) {
+            console.error('main | rabbitmq error', i, err);
+        }
+        await new Promise(resolve => setTimeout(resolve, 5000));
+    }
+    if (!publish) {
+        throw new Error('main | rabbitmq connection failed');
+    }
+
+    console.log('main | start generating IoT signals ...')
     while (true) {
         const mockedRecord = generateRandomRecord();
         await publish(mockedRecord);
 
+        // wait
         const sleepTimeout = Math.floor((Math.random() * 1000) + 500);
         await new Promise(resolve => setTimeout(resolve, sleepTimeout));
     }
--- producer/src/rabbitmq.js
+++ producer/src/rabbitmq.js
@@ -1,25 +1,30 @@
 import rabbitMQ from "amqplib";
 
-
 export async function initRabbitMQ() {
     const username = process.env.RABBITMQ_DEFAULT_USER;
     const password = process.env.RABBITMQ_DEFAULT_PASS;
     const hostname = process.env.RABBITMQ_HOST;
     const exchange = process.env.RABBITMQ_EXCHANGE;
     const signalRoutingKey = process.env.RABBITMQ_ROUTING_KEY_SIGNAL;
-    console.log('RabbitMQ is connecting to', hostname, exchange, signalRoutingKey)
+    console.log('RabbitMQ is connecting to', {
+        hostname,
+        exchange,
+        signalRoutingKey,
+        username,
+    })
 
     const conn = await rabbitMQ.connect({
         hostname,
         username,
         password,
     });
+    console.log('RabbitMQ Connected. creating channel ...',)
     const ch = await conn.createChannel();
     await ch.assertExchange(exchange, 'topic', {durable: false}); // delete the exchange on rabbit restart
     console.log('RabbitMQ connection started.');
 
     return async (payload, routingKey = signalRoutingKey) => {
         await ch.publish(exchange, routingKey, Buffer.from(JSON.stringify(payload), "utf8"));
-        console.log('publishing to', routingKey, Object.keys(payload)[0]);
+        // console.log('publishing to', routingKey, Object.keys(payload)[0]);
     }
 }
